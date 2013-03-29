cfbca34c78493884caf85cb3e0dd7e446894c4ae
Lurf Jurv <leijurv@gmail.com>
2013-03-29 10:19:28 -0700

    Now it can give you the overall strategy

--- Ghost/src/ghost/Ghost.java
+++ Ghost/src/ghost/Ghost.java
@@ -5,6 +5,7 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Ghost {
@@ -13,8 +14,7 @@ public class Ghost {
     static char[] alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     public static void load(String path) throws Exception {
-        FileInputStream fstream = new FileInputStream(path);
-        DataInputStream in = new DataInputStream(fstream);
+        DataInputStream in = new DataInputStream(new FileInputStream(path));
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
         String strLine;
         while ((strLine = br.readLine()) != null) {
@@ -32,25 +32,87 @@ public class Ghost {
         }
         return result;
     }
-
-    public static int[] solve(String sofar) {
+    public static void print(String sofar){
+        if (words.contains(sofar) && sofar.length()>3){
+            System.out.println(sofar);
+            return;
+        }
+        if (startsWith(sofar).isEmpty()){
+            return;
+        }
+        if (sofar.length()%2==0){
+            char next=alpha[solve(sofar,false)[1]];
+            String N=sofar+next;
+            System.out.println(sofar+":"+next);
+            print(N);
+        }else{
+            for (int i=0; i<alpha.length; i++){
+                print(sofar+alpha[i]);
+            }
+        }
+    }
+    static HashMap<String,int[]> cache=new HashMap<String,int[]>();
+    public static int[] solve(String sofar,boolean print) {
+        if (cache.get(sofar)!=null){
+            return cache.get(sofar);
+        }
         int M = sofar.length() % 2 == 0 ? 1 : -1;
         if ((words.contains(sofar) && sofar.length() > 3) || startsWith(sofar).isEmpty()) {
+            cache.put(sofar,new int[]{M, -1});
             return new int[]{M, -1};
         }
-        if (sofar.length() < 5) {
+        if (sofar.length() < 5 && print) {
             System.out.println(sofar);
         }
         for (int i = 0; i < alpha.length; i++) {
-            int[] q = solve(sofar + alpha[i]);
+            int[] q = solve(sofar + alpha[i],print);
             if (q[0] == M) {
+                cache.put(sofar,new int[]{q[0], i});
                 return new int[]{q[0], i};
             }
         }
+        cache.put(sofar,new int[]{-M, -1});
         return new int[]{-M, -1};
+    }
+    public static void prune(ArrayList<String> s){
+        for (int i=0; i<s.size(); i++){
+            String n=s.get(i);
+            if (n.length()>3){
+                ArrayList<String> N=startsWith(n);
+                N.remove(n);
+                s.removeAll(N);
+            }
+        }
+    }
+    public static void Run(String sofar){
+        
+        ArrayList<String> s = startsWith(sofar);
+        if (sofar.length()>1){
+        System.out.println("Searching for possible words in Ghost...");
+        prune(s);
+        System.out.println("Done");
+        }
+        if (s.isEmpty()) {
+            System.out.println("No words start with that.");
+            return;
+        }
+        System.out.println("Searching...");
+        int[] x = solve(sofar,true);
+        System.out.println(s.size() + " words that are possible in Ghost start with " + sofar + ": " + s);
+        System.out.println(x[0] == 1 ? "First player wins" : "Second player wins");
+        System.out.println(x[1] == -1 ? "Any move results in other player winning" : ("Optimal move is " + alpha[x[1]]));
 
     }
-
+    public static void Print(String sofar){
+        System.out.println("Solving...");
+        //solve(sofar);
+        System.out.println("Done. ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        print(sofar);
+    }
     public static void main(String[] args) {
         try {
             load("/Users/leijurv/Dropbox/Java-Projects/Ghost/Ghostwords.txt");
@@ -59,19 +121,18 @@ public class Ghost {
             System.out.println("There was an error. Maybe you didn't change the path on line 56?");
             return;
         }
-        Scanner scan = new Scanner(System.in);
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Overall strategy or just one? (1/2) >");
+        String s=scan.nextLine();
+        boolean b=true;
+        if (s.equals("1")){
+            b=false;
+        }
         System.out.print("What is it so far? >");
         String sofar = scan.nextLine();
-        ArrayList<String> s = startsWith(sofar);
-        if (s.isEmpty()) {
-            System.out.println("No words start with that.");
-            return;
-        }
-        System.out.println("Searching...");
-        int[] x = solve(sofar);
-        System.out.println(s.size() + " words start with " + sofar + ": " + s);
-        System.out.println(x[0] == 1 ? "First player wins" : "Second player wins");
-        System.out.println(x[1] == -1 ? "Any move results in other player winning" : ("Optimal move is " + alpha[x[1]]));
-
-    }
+if (b){
+    Run(sofar);
+}else{
+    Print(sofar);
 }
+}}
