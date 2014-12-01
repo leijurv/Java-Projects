9fe0c6178d82afdabad617f129e6bb8a5a60c001
Lurf Jurv <leijurv@gmail.com>
2014-11-30 19:34:15 -0800

    aklsjdghfjklasdgh

---
+++ Compiler/src/compiler/CommandIf.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiler;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author leijurv
+ */
+public class CommandIf extends Command{
+    Expression condition;
+    ArrayList<Object> ifTrue;
+    ArrayList<Object> ifFalse;
+    public CommandIf(Expression Condition, ArrayList<Object> IfTrue){
+        this(Condition,IfTrue,new ArrayList<>());
+    }
+    public CommandIf(Expression Condition, ArrayList<Object> IfTrue,ArrayList<Object> IfFalse){
+        condition=Condition;
+        ifTrue=IfTrue;
+        ifFalse=IfFalse;
+    }
+
+    @Override
+    public boolean execute(Context c){
+        Object exp=condition.evaluate(c);
+        Context local=c.subContext();
+        Boolean b=(Boolean)exp;
+        if (b){
+            for (Object o : ifTrue){
+                Command com=(Command)o;
+                if (com.execute(local)){
+                    c.Return(local.getReturn());
+                    return true;
+                }
+            }
+        }else{
+            for (Object o : ifFalse){
+                Command com=(Command)o;
+                if (com.execute(local)){
+                    c.Return(local.getReturn());
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
