83016c0a36d1b12d18e4c049d112da374a85c885
Lurf Jurv <leijurv@gmail.com>
2015-03-13 11:20:41 -0700

    I am literally a cat

--- Game of Life/src/main/Location.java
+++ Game of Life/src/main/Location.java
@@ -1,44 +1,41 @@
 package main;
-
-
-public class Location{
-        
-	int Row;
-	int Col;
-	public Location(int row, int col){
-		Row=row;
-		Col=col;
-                if (!Main.M.unbounded1){
+public class Location {
+    int Row;
+    int Col;
+    public Location(int row, int col) {
+        Row = row;
+        Col = col;
+        if (!Main.M.unbounded1) {
                     //Row=calcRow(Row,Main.minRow,Main.maxRow);
-                    //Col=calcRow(Col,Main.minCol,Main.maxCol);
-                }
-	}
-        public static int calcRow(int row,int minRow,int maxRow){
-            if (minRow!=0 && row<0){
-                return calcRow(row+minRow,minRow,maxRow);
-            }
-            if (maxRow!=0 && row>maxRow){
-                System.out.println(row+" "+maxRow);
-                return calcRow(row-maxRow,minRow,maxRow);
-            }
-            return row;
+            //Col=calcRow(Col,Main.minCol,Main.maxCol);
         }
-	public Location[] adjacent(){
-		Location[] result={new Location(Row-1,Col),//NORTH
-		new Location(Row-1,Col+1),//NORTHEAST
-		new Location(Row,Col+1),//EAST
-		new Location(Row+1,Col+1),//SOUTHEAST
-		new Location(Row+1,Col),//SOUTH
-		new Location(Row+1,Col-1),//SOUTHWEST
-		new Location(Row,Col-1),//WEST
-		new Location(Row-1,Col-1)};//NORTHWEST
-		return result;
-	}
-	public String toString(){
-		return "("+Row+","+Col+")";
-	}
-	public boolean equals(Object obk){
-		Location l=(Location)obk;
-		return (Row==l.Row && Col==l.Col);
-	}
+    }
+    public static int calcRow(int row, int minRow, int maxRow) {
+        if (minRow != 0 && row < 0) {
+            return calcRow(row + minRow, minRow, maxRow);
+        }
+        if (maxRow != 0 && row > maxRow) {
+            System.out.println(row + " " + maxRow);
+            return calcRow(row - maxRow, minRow, maxRow);
+        }
+        return row;
+    }
+    public Location[] adjacent() {
+        Location[] result = {new Location(Row - 1, Col),//NORTH
+            new Location(Row - 1, Col + 1),//NORTHEAST
+            new Location(Row, Col + 1),//EAST
+            new Location(Row + 1, Col + 1),//SOUTHEAST
+            new Location(Row + 1, Col),//SOUTH
+            new Location(Row + 1, Col - 1),//SOUTHWEST
+            new Location(Row, Col - 1),//WEST
+            new Location(Row - 1, Col - 1)};//NORTHWEST
+        return result;
+    }
+    public String toString() {
+        return "(" + Row + "," + Col + ")";
+    }
+    public boolean equals(Object obk) {
+        Location l = (Location) obk;
+        return (Row == l.Row && Col == l.Col);
+    }
 }
