8675ec32d661b022a88da84a34a4707a24c5466d
Lurf Jurv <leijurv@gmail.com>
2015-04-08 18:31:12 -1000

    Stegnawgraphie
    Stegnawgraphie

--- Stegnawgraphie/src/steganography/Steganography.java
+++ Stegnawgraphie/src/steganography/Steganography.java
@@ -12,13 +12,14 @@ import java.io.OutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.imageio.*;
-import javax.swing.filechooser.*;
+import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileFilter;
 /**
  *
  * @author leijurv
@@ -37,26 +38,29 @@ public class Steganography {
     static JLabel image = new JLabel("", new ImageIcon(new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)), JLabel.LEFT);
     static final int numBytes = 3;
     static final int numBitsModifiable = 3;
+    static final boolean writePayload = false;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         dataPanel.add(dataLabel);
         dataPanel.add(data);
         data.setBorder(BorderFactory.createLineBorder(Color.black));
-        final JFileChooser fc = new JFileChooser();
-        JFrame frame = new JFrame("Steganography");
+        JFrame frame = new JFrame("Stegnawgraphie");
         JComponent M = new JComponent() {
+            private static final long serialVersionUID = 1L;//because netbeans gets pissed if i dont have this
+            @Override
             public void paintComponent(Graphics g) {
                 g.drawString((seed == null) ? "Password not set" : "Password set to " + password, 10, 10);
                 if (withData != null && seed != null) {
                     String d = readString();
                     if (d != null) {
                         d = String.format("<html><div WIDTH=%d>%s</div><html>", (frame.getWidth() * 2) / 3, d);
-                        data.setText(d);
+                        data.setText(d);//YAY HTML IN JLABELS!!!!!
+                        //I'm going to start writing java app guis with CSS!!!!!
                         dataLabel.setText("Data:");
                     } else {
-                        data.setText(new String(doRead(100)));
+                        data.setText(new String(doRead(100)));//If it's unreadable, show the first 100 bytes in case only part of it is corrupted
                         dataLabel.setText("No data/unreadable data");
                     }
                 } else {
@@ -70,6 +74,7 @@ public class Steganography {
         open.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                final JFileChooser fc = new JFileChooser();
                 fc.addChoosableFileFilter(new ImageFilter());
                 fc.setAcceptAllFileFilterUsed(false);
                 int returnVal = fc.showDialog(open, "Cchuse a dank file pls");
@@ -79,16 +84,16 @@ public class Steganography {
                         originalImage = ImageIO.read(file);
                         withData = ImageIO.read(file);//TODO just clone the previous one
                         importLocation = file;
-                        exportLocation = new File(file.getParent() + File.separatorChar + file.getName().split("\\.")[0] + "Output.bmp");
-                        scaledOriginal = originalImage.getScaledInstance(200, -1, Image.SCALE_SMOOTH);
+                        exportLocation = new File(file.getParent() + File.separatorChar + file.getName().split("\\.")/* because split uses regex and . is a thing in regex that means stuff, escape it*/[0] + "Output.bmp");
+                        scaledOriginal = originalImage.getScaledInstance(200, -1, Image.SCALE_SMOOTH);//so apparently if you set one of the dimensions to -1 it preserves aspect ratio. if only i learned that a long time ago
                         image.setText("Original");
                         image.setIcon(new ImageIcon(scaledOriginal));
-                        image.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createLoweredBevelBorder()));
+                        image.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createLoweredBevelBorder()));//ooh fancy borders
                     } catch (IOException ex) {
                         Logger.getLogger(Steganography.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }//Silently ignore if user cancelled choose file dialog
-                frame.setSize(frame.getWidth() - 1, frame.getHeight() - 1);
+                frame.setSize(frame.getWidth() - 1, frame.getHeight() - 1);//the BEST way to get a JFrame to repaint!!!
             }
         });
         M.add(open);
@@ -103,8 +108,29 @@ public class Steganography {
                     JOptionPane.showMessageDialog(null, "where is image????? need import imag first!!!", "i cri evri tim", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
-                String data = JOptionPane.showInputDialog("Whatcha wanna encode?");
-                writeString(data);
+                String data1 = JOptionPane.showInputDialog("Whatcha wanna encode?");
+                double bytesBeingUsed = 4 + data1.length(); //DataOutputStream.writeUTF writes an int before the string to indicate the length
+                double capacity = numBytes * numBitsModifiable;
+                capacity *= image.getWidth();
+                capacity *= image.getHeight();
+                capacity /= 8;//converting capacity in bits to capacity in bytes
+                double usedUp = bytesBeingUsed / capacity;
+                usedUp = (int) (usedUp * 100 * 100);
+                usedUp /= 100;//round to 2 sig figs
+                String usagePercent = usedUp + "%";
+                //Not sure about these next 4 lines... it gives 100% if youve used more than like 3% of the capacity, doesn't seem right
+                double singleByteCollisionChance = bytesBeingUsed / capacity;
+                double singleByteNonCollisionChance = 1 - singleByteCollisionChance;
+                double nonCollisionChance = Math.pow(singleByteNonCollisionChance, bytesBeingUsed);
+                double collisionChance = 1 - nonCollisionChance;
+                collisionChance = (int) (collisionChance * 100 * 100);
+                collisionChance /= 100;
+                String collisionPercent = collisionChance + "%";
+                if (JOptionPane.showConfirmDialog(null,
+                        "That data is " + bytesBeingUsed + " bytes long, and will take up " + usagePercent + " of this image's " + capacity + " byte storage capacity. It has a " + collisionPercent + " chance of corrupting another message of the same length with a different password.", "choose pls", JOptionPane.YES_NO_OPTION) != JOptionPane.YES_OPTION) {
+                    return;
+                }
+                writeString(data1);
                 frame.repaint();
             }
         });
@@ -150,23 +176,6 @@ public class Steganography {
         frame.setSize(10000, 10000);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        /*
-         BufferedImage a = ImageIO.read(new File("/Users/leijurv/Downloads/dankOutput.bmp"));
-         long seed = 5021;
-         SteganographicOutputStream out = new SteganographicOutputStream(seed, a);
-         DataOutputStream o = new DataOutputStream(out);
-         SteganographicInputStream in = new SteganographicInputStream(seed, a);
-         DataInputStream i = new DataInputStream(in);
-         for (int x = 0; x < 10; x++) {
-         //o.writeUTF("Hello, I am currently 15 years old and I want to become a walrus. I know there’s a million people out there just like me, but I promise you I’m different. On December 14th, I’m moving to Antartica; home of the greatest walruses. I’ve already cut off my arms, and now slide on my stomach everywhere I go as training. I may not be a walrus yet, but I promise you if you give me a chance and the support I need, I will become the greatest walrus ever. If you have any questions or maybe advice, just inbox me. Thank you all so much ~~");//o.writeUTF("adoeuaoesntuhaetosaeou");
-         }
-         for (int x = 0; x < 10; x++) {
-         System.out.println(i.readUTF());
-         }
-         //ImageIO.write(a, "BMP", new File("/Users/leijurv/Downloads/dankOutput.bmp"));
-         /*System.out.println(read(r, a));
-         System.out.println(read(r, a));
-         System.out.println(read(r, a));*/
     }
     public static void writeString(String s) {
         SteganographicOutputStream out = new SteganographicOutputStream(seed, withData);
@@ -175,15 +184,31 @@ public class Steganography {
             o.writeUTF(s);
         } catch (IOException e) {
         }
+        if (writePayload) {
+            try(FileInputStream extraPayloadStream = new FileInputStream(new File("** some file =) **"))) {
+                byte[] payload = new byte[extraPayloadStream.available()];
+                extraPayloadStream.read(payload);
+                //TODO maybe write payload file name & extension as writeUTF here?
+                o.writeInt(payload.length);
+                o.write(payload);
+                JOptionPane.showMessageDialog(null, "wrote payload successfully", "wrote payload successfully", JOptionPane.INFORMATION_MESSAGE);
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+        }
     }
     public static String readString() {
         SteganographicInputStream in = new SteganographicInputStream(seed, withData);
         DataInputStream i = new DataInputStream(in);
+        String result;
         try {
-            return i.readUTF();
+            result = i.readUTF();
         } catch (Exception e) {
+            //result is already null
             return null;
         }
+        //TODO read payload if present
+        return result;
     }
     public static byte[] doRead(int numBytes) {
         SteganographicInputStream in = new SteganographicInputStream(seed, withData);
@@ -211,6 +236,7 @@ public class Steganography {
             byte x = (byte) b;
             writeData(r, a, x, modified);
         }
+        //TODO override some of the other write functions
     }
     public static class SteganographicInputStream extends InputStream {
         final Random r;
@@ -230,6 +256,7 @@ public class Steganography {
             }
             return X;
         }
+        //TODO override some of the other read functions
     }
     public static int[] getRandomLocation(Random r, BufferedImage a, boolean[][][] modified) {
         int x = r.nextInt(a.getWidth());
@@ -245,8 +272,12 @@ public class Steganography {
         return new int[] {x, y, bit};
     }
     public static void writeData(Random r, BufferedImage a, byte b, boolean[][][] modified) {
+        byte[] mask = new byte[1];
+        r.nextBytes(mask);
+        b ^= mask[0];//So now the bits it's writing are evenly distributed 1s and 0s, because it was xored with random-ish data
+        //this is NSA grade encryption here. just xor your data with the output of java.util.random. the password is the seed to the RNG.
         for (int i = 0; i < 8; i++) {
-            boolean bit = ((b >> i) & 1) == 1;
+            int bit = ((b >> i) & 1);
             //System.out.println("Bit " + i + " is " + bit);
             int[] location = getRandomLocation(r, a, modified);
             int x = location[0];
@@ -259,7 +290,9 @@ public class Steganography {
         }
     }
     public static byte readData(Random r, BufferedImage a, boolean[][][] modified) {
-        byte c = 0;
+        byte[] mask = new byte[1];
+        r.nextBytes(mask);//WHY doesn't Random have a readByte function????? I just want one byte without having to create a whole new array!
+        byte c = mask[0];
         for (int i = 0; i < 8; i++) {
             int[] location = getRandomLocation(r, a, modified);
             int x = location[0];
@@ -268,7 +301,7 @@ public class Steganography {
             int color = a.getRGB(x, y);
             byte result = (byte) ((color & (1 << position)) >> position);
             //System.out.println("Bit " + i + " at " + x + "," + y + " is " + result + " color " + color);
-            c |= (result << i);
+            c ^= (result << i);
         }
         //System.out.println("Read byte " + c);
         return c;
@@ -276,42 +309,40 @@ public class Steganography {
     public static int getBit(int ID, int position) {
         return ((ID >> position) & 1);
     }
-    public static int setBit(int ID, int position, boolean set) {
-        return (ID & (~(1 << position))) | ((set ? 1 : 0) << position);
+    public static int setBit(int ID, int position, int set) {
+        return (ID & (~(1 << position))) | (set << position);
     }
     public static long seedFromString(String s) {
-        long seed = 0;
-        for (int i = 0; i < s.length(); i++) {
-            seed += s.charAt(i) << (i * 7);
+        long seedGen = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            seedGen += chars[i] << (i * 7);
 //Pretty much fills up seed with the contents of the string, with a little overlap if the string is more that 8 bytes
         }
-        return seed;
+        return seedGen;
     }
     public static class ImageFilter extends FileFilter {
-        //Accept all directories and all gif, jpg, tiff, or png files.
+        @Override
         public boolean accept(File f) {
             if (f.isDirectory()) {
                 return false;
             }
             String extension = getExtension(f);
             if (extension != null) {
-                if (extension.equals("tiff")
-                        || extension.equals("tif")
-                        || extension.equals("gif")
-                        || extension.equals("jpeg")
-                        || extension.equals("jpg")
-                        || extension.equals("png") || extension.equals("bmp")) {
-                    return true;
-                } else {
-                    return false;
+                String[] possibilities = ImageIO.getReaderFileSuffixes();//Only accept files that ImageIO can read
+                for (String ext : possibilities) {
+                    if (ext.equalsIgnoreCase(extension)) {
+                        return true;
+                    }
                 }
+                return false;
             }
             return false;
         }
         //The description of this filter
         @Override
         public String getDescription() {
-            return "Just Images";
+            return "Just images that ImageIO can handle";
         }
         public static String getExtension(File f) {
             String ext = null;
